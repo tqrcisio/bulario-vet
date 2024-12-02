954f3044b8f0477bf905f6e2620b884b4f955df1
Tarcisio Ferreira <tarcisiosaf@gmail.com>
2024-12-02 18:41:34 -0300

    feat(typeorm): add typeorm settings, with ConfigService from nestjs to handle enviroment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { PostgresConfigService } from './database/postgres.config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
