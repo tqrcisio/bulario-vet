5b8bdeffc1e2489133eb406306283145893d52da
Tarcisio Ferreira <tarcisiosaf@gmail.com>
2024-12-02 21:22:16 -0300

    feat(user.controller): implemented patch and  delete endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Delete,
+  Param,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ListUserDto } from './dto/list-user.dto';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @ApiTags('User')
 @Controller('user')
@@ -11,16 +20,27 @@ export class UserController {
 
   @Post()
   @ApiOperation({ summary: 'Create a new user' })
-  async create(@Body() createUserDto: CreateUserDto) {
-    const user = await this.userService.create(createUserDto);
-    const newUser = new ListUserDto(user.id, user.name);
+  async create(@Body() userData: CreateUserDto) {
+    const createdUser = await this.userService.create(userData);
+    const newUser = new ListUserDto(createdUser.id, createdUser.name);
     return newUser;
   }
+
   @Get()
   @ApiOperation({ summary: 'List all users' })
-  listAll() {
-    return this.userService.findAll();
+  async listAll() {
+    return await this.userService.findAll();
   }
 
-  // TODO: implement update, delete, get by id/email endpoints
+  @Patch(':id')
+  @ApiOperation({ summary: 'Update a user' })
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete a user' })
+  delete(@Param('id') id: string) {
+    return this.userService.deleteUser(id);
+  }
 }
